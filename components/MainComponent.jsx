c3d7150090f3faf1ff0156f90b880f4f77e3b5d4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-20 09:58:11 -0500

    Custom Side Drawer

--- components/MainComponent.jsx
+++ components/MainComponent.jsx
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Home from "./Home";
-import { View, Platform, StyleSheet } from "react-native";
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+} from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import Constants from "expo-constants";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Icon } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -120,6 +128,29 @@ const AboutNavigator = createStackNavigator(
     }),
   }
 );
+
+const CustomDrawerContentComponent = (props) => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./images/logo.png")}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>Nucamp</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+
 const MainNavigator = createDrawerNavigator(
   {
     Home: {
@@ -169,6 +200,7 @@ const MainNavigator = createDrawerNavigator(
   },
   {
     drawerBackgroundColor: "#CEC8FF",
+    contentComponent: CustomDrawerContentComponent,
   }
 );
 
@@ -190,6 +222,28 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: "#5637DD",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+  },
+  drawerHeaderText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60,
+  },
+
   stackIcon: {
     marginLeft: 10,
     color: "#fff",
