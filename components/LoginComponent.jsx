a4227b3ef8d7786b3bc091b34c8df296ff404795
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-04 08:38:13 -0500

    LoginComponent

---
+++ components/LoginComponent.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { Input, Checkbox } from "react-native-elements";
+import * as SecureStore from "expo-secure-store";
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      remember: false,
+    };
+  }
+
+  static navigationoptions = {
+    title: "Login",
+  };
+
+  hanldeLogin() {
+    console.log(JSON.stringify(this.state));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Input
+          placeholder="Username"
+          leftIcon={{ type: "font-awesome", name: "user=o" }}
+          onChangeText={(username) => this.setState({ username })}
+          value={this.state.username}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "key" }}
+          onChangeText={(password) => this.setState({ password })}
+          value={this.state.password}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+
+        <Checkbox
+          title="Remember Me"
+          center
+          checked={this.state.remember}
+          onPress={() => this.setState({ remember: !this.state.remember })}
+          containerStyle={styles.formCheckbox}
+        />
+        <View style={styles.formButton}>
+          <Button
+            onPress={() => this.hanldeLogin()}
+            title="Login"
+            color="#5637DD"
+          ></Button>
+        </View>
+      </View>
+    );
+  }
+}
